583fca8e66b2cadb13bd7e648506acef3d1a06bb
gustodse <gustodse@student.gu.se>
2021-11-25 16:01:31 +0100

    Steg 3 - sista inlämningen

---
+++ lib/add_todo.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'todo.dart';
+
+class AddTodo extends StatefulWidget {
+  const AddTodo({Key? key, required this.todo}) : super(key: key);
+
+  final TodoItem todo;
+
+  @override
+  State<AddTodo> createState() => AddTodoState(todo);
+}
+
+class AddTodoState extends State<AddTodo> {
+  late String attGora;
+  late bool done;
+
+  late TextEditingController textEditingController;
+
+  AddTodoState(TodoItem sak) {
+    this.attGora = sak.attGora;
+    this.done = sak.done;
+
+    textEditingController = TextEditingController(text: sak.attGora);
+
+    textEditingController.addListener(() {
+      setState(() {
+        attGora = textEditingController.text;
+      });
+    });
+  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Lägg till TODO"), actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.pop(
+                    context, TodoItem(attGora: attGora, done: done, id: "id"));
+              },
+              child: Text(
+                "ADD",
+                style: TextStyle(color: Colors.white),
+              ))
+        ]),
+        body: Center(
+          child: Container(
+              margin: const EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 3)),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: TextField(
+                    decoration: const InputDecoration(
+                        contentPadding: EdgeInsets.only(left: 20)),
+                    controller: textEditingController),
+              )),
+        ));
+  }
+}
---
+++ lib/todo.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:min_app/api.dart';
+
+class TodoItem {
+  String attGora;
+  bool done = false;
+  String id = "du har överskrivit idt";
+
+  TodoItem({
+    required this.attGora,
+    required this.done,
+    required this.id,
+  });
+
+  static Map<String, dynamic> toJson(TodoItem sak) {
+    return {
+      "title": sak.attGora,
+      "done": sak.done,
+      "id": sak.id,
+    };
+  }
+
+  static TodoItem fromJson(Map<String, dynamic> json) {
+    return TodoItem(
+      attGora: json["title"],
+      done: json["done"],
+      id: json["id"],
+    );
+  }
+}
+
+class MyState extends ChangeNotifier {
+  List<TodoItem> list = [];
+
+  List<TodoItem> get lista => list;
+
+  String _filterBy = "Alla";
+
+  String get filterBy => _filterBy;
+
+  Future fetchTodo() async {
+    List<TodoItem> lista = await Api.fetchTodo();
+    list = lista;
+    notifyListeners();
+  }
+
+  void addItem(TodoItem sak) async {
+    await Api.addTodo(sak);
+    await fetchTodo();
+    notifyListeners();
+  }
+
+  void addTodo(TodoItem sak) {
+    list.add(sak);
+    notifyListeners();
+  }
+
+  void removeItem(TodoItem sak) async {
+    await Api.deleteTodos(sak.id);
+    await fetchTodo();
+    notifyListeners();
+  }
+
+  void changeState(TodoItem sak, bool done) async {
+    sak.done = !sak.done;
+    await Api.updateTodos(sak, sak.id);
+    await fetchTodo();
+  }
+
+  void setFilterBy(String filterBy) {
+    this._filterBy = filterBy;
+    notifyListeners();
+  }
+}
